fc3afec7be4d653400ceba503abd922a2945a9db
rcmendes2025 <124199152+rcmendes2025@users.noreply.github.com>
2023-02-22 13:56:22 -0500

    Write function with position and color
    https://editor.p5js.org/ryan.mendes1/sketches/JYVgCzHgf

---
+++ excercise_three.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  noStroke();
+  
+//Called Functions
+  myTriangle(200, 200, 50, 10, 350, 10, "red");
+  myEllipse(200, 200, 150, 150, "#FFC107");
+  myArc(200, 200, 150, 150, 230, 310, "black");
+}
+  
+//My Functions
+function myTriangle(x1, y1, x2, y2, x3, y3, myColor) {
+  fill(myColor);
+  triangle(x1, y1, x2, y2, x3, y3);
+}
+  
+function myEllipse(x, y, w, h, myColor) {
+  fill(myColor);
+  ellipse(x, y, h, w);
+}
+
+function myArc(x, y, w, h, start, stop, myColor) {
+  fill(myColor);
+  angleMode(DEGREES);
+  arc(x, y, w, h, start, stop);
+}
